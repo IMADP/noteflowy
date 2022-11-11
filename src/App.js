8274cbc53ff101f769062d501f504036bfdba443
Anthony DePalma <antdepalma@gmail.com>
2022-11-10 19:54:08 -0500

    uuids

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Sidebar from './components/Sidebar';
 import Header from './components/Header';
 import Notes from './components/Notes';
 import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 function App() {
   const [notes, setNotes] = useState([{
@@ -17,7 +18,7 @@ function App() {
   };
 
   const onAdd = (note) => {
-    note.id = Math.floor(Math.random() * 10000);
+    note.id = uuidv4(); 
     setNotes([...notes, note]);
   };
 
