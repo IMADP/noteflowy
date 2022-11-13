2d03b0cdfc0a8901b6510fda6fea048710d0bd11
Anthony DePalma <antdepalma@gmail.com>
2022-11-13 13:19:03 -0500

    delete confirmation

--- src/components/notes/NoteDeleteDialog.js
+++ src/components/notes/NoteDeleteDialog.js
@@ -2,25 +2,24 @@ import React from 'react';
 import * as AlertDialog from '@radix-ui/react-alert-dialog';
 import './NoteDeleteDialog.css';
 
-const NoteDeleteDialog = ({note, onDelete}) => (
+const NoteDeleteDialog = ({ children, note, onDelete }) => (
   <AlertDialog.Root>
     <AlertDialog.Trigger asChild>
-      <button className="Button violet">Delete note</button>
+      {children}
     </AlertDialog.Trigger>
     <AlertDialog.Portal>
       <AlertDialog.Overlay className="AlertDialogOverlay" />
       <AlertDialog.Content className="AlertDialogContent">
-        <AlertDialog.Title className="AlertDialogTitle">Are you absolutely sure?</AlertDialog.Title>
+        <AlertDialog.Title className="AlertDialogTitle">Are you sure?</AlertDialog.Title>
         <AlertDialog.Description className="AlertDialogDescription">
-          This action cannot be undone. This will permanently delete your account and remove your
-          data from our servers.
+          This will delete the note and subnotes.
         </AlertDialog.Description>
         <div style={{ display: 'flex', gap: 25, justifyContent: 'flex-end' }}>
           <AlertDialog.Cancel asChild>
             <button className="Button mauve">Cancel</button>
           </AlertDialog.Cancel>
           <AlertDialog.Action asChild>
-            <button className="Button red" onClick={() => onDelete(note.id)}>Yes, delete account</button>
+            <button className="Button red" onClick={() => onDelete(note.id)}>Yes, delete note</button>
           </AlertDialog.Action>
         </div>
       </AlertDialog.Content>
--- src/components/notes/NoteMenu.js
+++ src/components/notes/NoteMenu.js
@@ -1,24 +1,14 @@
 import React from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import {
-  HamburgerMenuIcon,
-    DotFilledIcon,
-    CheckIcon,
-    ChevronRightIcon,
-  } from '@radix-ui/react-icons';
-  import './NoteMenu.css';
+  HamburgerMenuIcon
+} from '@radix-ui/react-icons';
+import './NoteMenu.css';
 import NoteDeleteDialog from './NoteDeleteDialog';
 
-import * as AlertDialog from '@radix-ui/react-alert-dialog';
-import './NoteDeleteDialog.css';
 
+const DropdownMenuDemo = ({ note, onDelete }) => {
 
-const DropdownMenuDemo = ({note, onDelete}) => {
-  const [bookmarksChecked, setBookmarksChecked] = React.useState(true);
-  const [urlsChecked, setUrlsChecked] = React.useState(false);
-  const [person, setPerson] = React.useState('pedro');
-const [isDeleteOpen, setIsDeleteOpen] = React.useState(false);
-  
   return (
     <DropdownMenu.Root>
       <DropdownMenu.Trigger asChild>
@@ -29,11 +19,11 @@ const [isDeleteOpen, setIsDeleteOpen] = React.useState(false);
 
       <DropdownMenu.Portal>
         <DropdownMenu.Content className="DropdownMenuContent" sideOffset={5}>
-          <DropdownMenu.Item className="DropdownMenuItem" onSelect={() => onDelete(note.id)}>
-            Delete Note <div className="RightSlot"></div>
-          </DropdownMenu.Item>
-            
-
+          <NoteDeleteDialog note={note} onDelete={onDelete}>
+            <DropdownMenu.Item className="DropdownMenuItem" onSelect={(event) => event.preventDefault()}>
+              Delete Note <div className="RightSlot"></div>
+            </DropdownMenu.Item>
+          </NoteDeleteDialog>
         </DropdownMenu.Content>
       </DropdownMenu.Portal>
     </DropdownMenu.Root>
