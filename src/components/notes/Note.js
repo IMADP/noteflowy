0dd8bd29811dfa394b58605e53303198d8edb33a
Jonathan Hall <jonathan.edward.hall@gmail.com>
2022-11-16 11:20:39 -0500

    Remove excess spans. Use flex display on Note component.

--- src/components/notes/Note.js
+++ src/components/notes/Note.js
@@ -1,30 +1,27 @@
 import React, { useRef } from "react";
 import NoteMenu from "./NoteMenu";
 import Editable from "../global/Editable";
+import "./Note.css";
 
 function Note({ note, onAdd, onUpdate, onDelete }) {
     const inputRef = useRef();
 
     return (
-        <div>
-            <span style={{marginRight: '10px'}}>
-                <NoteMenu note={note} onAdd={onAdd} onUpdate={onUpdate} onDelete={onDelete} />
-            </span>
-            <span >
-                <Editable
-                    text={note.text}
-                    childRef={inputRef}
-                    type="input"
-                    >
-                    <input
-                        ref={inputRef}
-                        type="text"
-                        name="task"
-                        value={note.text}
-                        onChange={(e) => onUpdate({...note, text: e.target.value})}
-                    />
-                    </Editable>
-            </span>
+        <div className="Note">
+            <NoteMenu note={note} onAdd={onAdd} onUpdate={onUpdate} onDelete={onDelete} />
+            <Editable
+                text={note.text}
+                childRef={inputRef}
+                type="input"
+            >
+                <input
+                    ref={inputRef}
+                    type="text"
+                    name="task"
+                    value={note.text}
+                    onChange={(e) => onUpdate({...note, text: e.target.value})}
+                />
+            </Editable>
         </div>
     );
 }
