285a59b1429f5ce9d447535154695c4b675168b3
Anthony DePalma <antdepalma@gmail.com>
2022-11-20 17:13:30 -0500

    search placeholder

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,11 +1,8 @@
-
-import { Link } from "react-router-dom";
-
 function Header() {
  
   return (
     <header>
-      
+      <input type='text' style={{width: '100%', height: '100%'}} placeholder="Search...(doesn't work yet)" />
     </header>
   );
 } 
