2b3b3ab382686d62a3a7f9673738828afa015dce
Anthony DePalma <antdepalma@gmail.com>
2022-11-22 07:20:44 -0500

    reset search bar

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,17 +1,40 @@
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 function Header() {
   const navigate = useNavigate();
+  const location = useLocation();
+  const [query, setQuery] = useState('');
 
-  // TODO: Clear input state on url change away from /search
-  
+  // this effect will reset the search bar when navigating away
+  useEffect(() => {
+    const paths = location.pathname.split('/');
+    const isSearchPath = paths[1] === 'search';
+
+    if(!isSearchPath) {
+      setQuery('');
+    }
+    
+  }, [location]);
+
+  /**
+   * Sets the query from the input and navigates to a search url.
+   * 
+   * @param {*} q 
+   */
+  const onChange = (q) => {
+    setQuery(q);
+    navigate(`/search/${q}`);
+  };
+
   return (
     <header>
-      <input type='text' style={{width: '100%', height: '100%'}} 
+      <input type='text' style={{ width: '100%', height: '100%' }}
         placeholder="Search..."
-        onChange={(e) => navigate(`/search/${e.target.value}`)} />
+        value = {query}
+        onChange={(e) => onChange(e.target.value)} />
     </header>
   );
-} 
+}
 
 export default Header;
