8a181e68f362af5bf82672e4878c1a01dad1a2fc
Anthony DePalma <antdepalma@gmail.com>
2022-11-23 21:14:35 -0500

    moving notes logic to parent

--- src/components/content-notes/Notes.js
+++ src/components/content-notes/Notes.js
@@ -1,79 +1,13 @@
 import { DoubleArrowUpIcon } from '@radix-ui/react-icons';
-import { Link, useLocation, useSearchParams } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Note from './Note';
 import './Notes.css';
 import NotesAdd from './NotesAdd';
 import NotesCollapse from './NotesCollapse';
-import { clone, findNote,  visitNoteTreeReverse } from '../appUtil';
 
-function Notes({ notes, noteActions }) {
-  const [searchParams] = useSearchParams();
- 
+function Notes({ notes, parent, noteActions }) {
   const paths = useLocation().pathname.split('/');
   const isRootPath = paths.length === 2;
-  const isNotePath = paths[1] === 'note';
-  const search = searchParams.get('search');
-
-  let parent;
-
-  // TODO: This component needs to be broken up into several smaller ones
-
-  // filter note by path
-  if (isNotePath) {
-    const result = findNote(notes, paths[2]);
-    parent = result.parent;
-
-    if (result.note) {
-      notes = [result.note];
-    }
-  }
-
-  // filter notes by search
-  if (search != null) {
-    const term = decodeURI(search).toUpperCase();
-    const clonedNotes = clone(notes);
-
-    // start at the child nodes to filter non-matches and mark to keep parents to preserve the path
-    visitNoteTreeReverse(clonedNotes, (currentNote) => {
-
-      // search for a match on the note itself and mark it as keep
-      const textFound = currentNote.text != null && currentNote.text.toUpperCase().includes(term);
-      const detailsFound = currentNote.details != null && currentNote.details.toUpperCase().includes(term);
-      
-      if(textFound || detailsFound) {
-        currentNote.keep = true;
-        currentNote.collapsed = false;
-      }
-
-      // filter any children who are not matches or parents of matches
-      currentNote.children = currentNote.children.filter((c) => {
-
-        // keep notes marked by child matches
-        if(c.keep) {
-          currentNote.keep = true;
-          currentNote.collapsed = false;
-          return true;
-        }
-
-        // search for text matches
-        const textFound = c.text != null && c.text.toUpperCase().includes(term);
-        const detailsFound = c.details != null && c.details.toUpperCase().includes(term);
-      
-        // keep matches and mark parent to be kept
-        if(textFound || detailsFound) {
-          currentNote.keep = true;
-          currentNote.collapsed = false;
-          return true;
-        }
-
-        return false;
-      });
-
-    });
-
-    // finally trim out any parent nodes that weren't marked to keep
-    notes = clonedNotes.filter((n) => n.keep);
-  }
 
   return (
     <main className="Notes content">
--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,11 +1,87 @@
+import { useLocation, useSearchParams } from 'react-router-dom';
+import { clone, findNote, visitNoteTreeReverse } from '../appUtil';
 import Header from "../content-header/Header";
 import Notes from "../content-notes/Notes";
 
+/**
+ * Content
+ * 
+ * This component is primarily responsible for providing the notes array to child components.
+ * It will inspect the url to determine the parent note, and filter based on search parameters.
+ * 
+ * @param {*} param0 
+ * @returns 
+ */
 function Content({ notes, noteActions }) {
+  const paths = useLocation().pathname.split('/');
+  const isNotePath = paths[1] === 'note';
+  let parent = null;
+
+  // if this is not the root path, find the matching note from the url
+  if (isNotePath) {
+    const result = findNote(notes, paths[2]);
+    parent = result.parent;
+
+    if (result.note) {
+      notes = [result.note];
+    }
+  }
+
+  // if search params were provided, clone and filter out matching notes
+  const [searchParams] = useSearchParams();
+  const search = searchParams.get('search');
+
+  // filter notes by search
+  if (search != null) {
+    const term = decodeURI(search).toUpperCase();
+    const clonedNotes = clone(notes);
+
+    // start at the child nodes to filter non-matches and mark to keep parents to preserve the path
+    visitNoteTreeReverse(clonedNotes, (currentNote) => {
+
+      // search for a match on the note itself and mark it as keep
+      const textFound = currentNote.text != null && currentNote.text.toUpperCase().includes(term);
+      const detailsFound = currentNote.details != null && currentNote.details.toUpperCase().includes(term);
+
+      if (textFound || detailsFound) {
+        currentNote.keep = true;
+        currentNote.collapsed = false;
+      }
+
+      // filter any children who are not matches or parents of matches
+      currentNote.children = currentNote.children.filter((c) => {
+
+        // keep notes marked by child matches
+        if (c.keep) {
+          currentNote.keep = true;
+          currentNote.collapsed = false;
+          return true;
+        }
+
+        // search for text matches
+        const textFound = c.text != null && c.text.toUpperCase().includes(term);
+        const detailsFound = c.details != null && c.details.toUpperCase().includes(term);
+
+        // keep matches and mark parent to be kept
+        if (textFound || detailsFound) {
+          currentNote.keep = true;
+          currentNote.collapsed = false;
+          return true;
+        }
+
+        return false;
+      });
+
+    });
+
+    // finally trim out any parent nodes that weren't marked to keep
+    notes = clonedNotes.filter((n) => n.keep);
+  }
+
   return (
     <>
       <Header />
-      <Notes notes={notes} noteActions={noteActions} />
+      <Notes notes={notes} parent={parent} noteActions={noteActions} />
     </>
   );
 }
