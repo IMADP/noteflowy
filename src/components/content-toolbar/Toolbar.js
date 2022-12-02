c9bdaa157a27dc335983d36850939d99118c189b
Anthony DePalma <antdepalma@gmail.com>
2022-12-02 14:39:03 -0500

    move to root drop

--- src/components/content-toolbar/Toolbar.js
+++ src/components/content-toolbar/Toolbar.js
@@ -1,11 +1,13 @@
 import CollapseButton from './CollapseButton';
 import LockButton from './LockButton';
 import UpButton from './UpButton';
+import DropButton from './DropButton';
 import './Toolbar.css';
 
 function Toolbar({ note, parentUrl, noteActions }) {
   return (
     <div className='toolbar'>
+      <DropButton note={note} noteActions={noteActions} />
       <UpButton parentUrl={parentUrl} />
       <LockButton note={note} noteActions={noteActions} />
       <CollapseButton note={note} noteActions={noteActions} />
