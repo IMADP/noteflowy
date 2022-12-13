3a332288167f92671231a1c9637f7755b8c8ffbc
Anthony DePalma <antdepalma@gmail.com>
2022-12-13 14:11:51 -0500

    left and right click menu buttons

--- src/feature/notes/note-menu.tsx
+++ src/feature/notes/note-menu.tsx
@@ -1,5 +1,6 @@
 import { Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BiBullseye } from 'react-icons/bi';
+import { useNavigate } from 'react-router-dom';
 import { Note, useNotes } from './use-notes';
 
 interface NoteMenuProps {
@@ -9,27 +10,33 @@ interface NoteMenuProps {
 
 export const NoteMenu = ({ note, noteParent }: NoteMenuProps) => {
   const notes = useNotes();
+  const navigate = useNavigate();
+
+  const onRightClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    navigate(`/note/${note.id}`);
+  };
 
   return (
-    <Menu>
-      <MenuButton p={2} aria-label='Options'>
+    <Menu  >
+      <MenuButton p={2} aria-label='Options' onContextMenu={onRightClick} >
         <BiBullseye />
       </MenuButton>
       <MenuList>
-        <MenuItem onClick={() => notes.onAddSubNote(note)}>
+        <MenuItem closeOnSelect={true} onClick={() => notes.onAddSubNote(note)}>
           Add Sub Note
         </MenuItem>
         <MenuItem onClick={() => notes.onDuplicate(noteParent, note)}>
-         Duplicate Note
+          Duplicate Note
         </MenuItem>
         <MenuItem onClick={() => notes.onUpdate({ ...note, locked: !note.locked })}>
-        {note.locked ? 'Unlock' : 'Lock'}
+          {note.locked ? 'Unlock' : 'Lock'}
         </MenuItem>
         <MenuItem onClick={() => notes.onUpdate({ ...note, completed: !note.completed })}>
-        {note.completed ? 'Uncomplete' : 'Complete'} 
+          {note.completed ? 'Uncomplete' : 'Complete'}
         </MenuItem>
         <MenuItem onClick={() => notes.onUpdate({ ...note, showDetails: !note.showDetails })}>
-        {note.showDetails ? 'Hide Details' : 'Show Details'}
+          {note.showDetails ? 'Hide Details' : 'Show Details'}
         </MenuItem>
       </MenuList>
     </Menu>
