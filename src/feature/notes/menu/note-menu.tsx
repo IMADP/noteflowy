57a2d73762e374532916567b99cb85838c3176ee
Anthony DePalma <antdepalma@gmail.com>
2022-12-14 15:55:17 -0500

    lock icon

--- src/feature/notes/menu/note-menu.tsx
+++ src/feature/notes/menu/note-menu.tsx
@@ -1,5 +1,5 @@
 import { Menu, MenuButton, MenuDivider, MenuList } from '@chakra-ui/react';
-import { BiBullseye } from 'react-icons/bi';
+import { BiBullseye, BiLockOpen, BiLockOpenAlt } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
 import { NoteMenuAdd } from './note-menu-add';
 import { NoteMenuComplete } from './note-menu-complete';
@@ -26,18 +26,30 @@ export const NoteMenu = ({ note, noteParent }: NoteMenuProps) => {
   return (
     <Menu>
       <MenuButton p={2} aria-label='Options' onContextMenu={onRightClick} >
-        <BiBullseye />
+        {note.locked ? <BiLockOpen /> : <BiBullseye />}
       </MenuButton>
       <MenuList>
-        <NoteMenuAdd note={note} />
-        <NoteMenuDuplicate note={note} noteParent={noteParent} />
-        <MenuDivider />
-        <NoteMenuLock note={note} />
-        <NoteMenuComplete note={note} />
-        <NoteMenuDetails note={note} />
-        <NoteMenuLink note={note} />
-        <MenuDivider />
-        <NoteMenuDelete note={note} />
+
+        {!note.locked &&
+          <>
+            <NoteMenuAdd note={note} />
+            <NoteMenuDuplicate note={note} noteParent={noteParent} />
+            <MenuDivider />
+            <NoteMenuLock note={note} />
+            <NoteMenuComplete note={note} />
+            <NoteMenuDetails note={note} />
+            <NoteMenuLink note={note} />
+            <MenuDivider />
+            <NoteMenuDelete note={note} />
+          </>
+        }
+
+        {note.locked &&
+          <>
+            <NoteMenuLock note={note} />
+          </>
+        }
+        
       </MenuList>
     </Menu>
   )
