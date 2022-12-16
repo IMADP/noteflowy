356421660da904ef2ed389970c34634cc9c5190e
Anthony DePalma <antdepalma@gmail.com>
2022-12-16 11:49:01 -0500

    drag drop

--- src/feature/notes/menu/note-menu.tsx
+++ src/feature/notes/menu/note-menu.tsx
@@ -1,6 +1,9 @@
 import { Menu, MenuButton, MenuDivider, MenuList } from '@chakra-ui/react';
-import { BiBullseye, BiLockOpen, BiLockOpenAlt } from 'react-icons/bi';
+import { useDrag, useDrop } from 'react-dnd';
+import { BiBullseye, BiLockOpen } from 'react-icons/bi';
 import { useNavigate } from 'react-router-dom';
+import { findNote, isDescendent } from '../notes-util';
+import { Note, useNotes } from '../use-notes';
 import { NoteMenuAdd } from './note-menu-add';
 import { NoteMenuComplete } from './note-menu-complete';
 import { NoteMenuDelete } from './note-menu-delete';
@@ -8,7 +11,6 @@ import { NoteMenuDetails } from './note-menu-details';
 import { NoteMenuDuplicate } from './note-menu-duplicate';
 import { NoteMenuLink } from './note-menu-link';
 import { NoteMenuLock } from './note-menu-lock';
-import { Note } from '../use-notes';
 
 interface NoteMenuProps {
   note: Note;
@@ -16,7 +18,53 @@ interface NoteMenuProps {
 }
 
 export const NoteMenu = ({ note, noteParent }: NoteMenuProps) => {
+  const notes = useNotes();
   const navigate = useNavigate();
+
+  // get the drag and preview refs
+  const [, drag,] = useDrag(() => ({
+    type: 'note',
+    item: { id: note.id },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging()
+    })
+
+  }));
+
+  // determines if a dragged note can be nested under the current note
+  const canDropFunction = (draggedNote: Note) => {
+
+    // don't allow dragging to itself
+    if (draggedNote.id === note.id) {
+      return false;
+    }
+
+    const results = findNote(notes.rootNote, draggedNote.id);
+
+    // this shouldn't happen
+    if (!results) {
+      return false;
+    }
+
+    // don't allow dragging to direct parent
+    if (note.children.find(n => n.id === draggedNote.id)) {
+      return false;
+    }
+
+    // don't allow a parent note to be dragged into a descendent
+    return !isDescendent(results.note, note.id);
+  }
+
+  const [, drop] = useDrop(
+    () => ({
+      accept: 'note',
+      drop: (droppedNote) => notes.onMove(droppedNote.id, note.id),
+      canDrop: canDropFunction,
+      collect: (monitor) => ({
+        canDrop: !!monitor.canDrop()
+      })
+    }), [note]
+  );
 
   const onRightClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     event.preventDefault();
@@ -25,7 +73,7 @@ export const NoteMenu = ({ note, noteParent }: NoteMenuProps) => {
 
   return (
     <Menu>
-      <MenuButton p={2} aria-label='Options' onContextMenu={onRightClick} >
+      <MenuButton p={2} aria-label='Options' onContextMenu={onRightClick} ref={(node) => drag(drop(node))}>
         {note.locked ? <BiLockOpen /> : <BiBullseye />}
       </MenuButton>
       <MenuList>
@@ -49,7 +97,7 @@ export const NoteMenu = ({ note, noteParent }: NoteMenuProps) => {
             <NoteMenuLock note={note} />
           </>
         }
-        
+
       </MenuList>
     </Menu>
   )
--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import { Navigation } from 'feature/navigation/navigation';
 import { NotesContent } from 'feature/notes/notes-content';
 import { NotesProvider } from 'feature/notes/use-notes';
 import React from 'react';
+import { DndProvider } from 'react-dnd';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from "react-router-dom";
 import './index.css';
@@ -17,20 +19,22 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Router>
-      <ChakraProvider theme={theme}>
-        <NotesProvider>
-          <Box height="100vh" overflow="hidden" position="relative">
-            <Flex h="full" id="app-container">
-              <Navigation />
-              <Stack flex="1" overflow="auto" spacing="0">
-                <Header />
-                <Divider />
-                <NotesContent/>
-              </Stack>
-            </Flex>
-          </Box>
-        </NotesProvider>
-      </ChakraProvider>
+      <DndProvider backend={HTML5Backend}>
+        <ChakraProvider theme={theme}>
+          <NotesProvider>
+            <Box height="100vh" overflow="hidden" position="relative">
+              <Flex h="full" id="app-container">
+                <Navigation />
+                <Stack flex="1" overflow="auto" spacing="0">
+                  <Header />
+                  <Divider />
+                  <NotesContent />
+                </Stack>
+              </Flex>
+            </Box>
+          </NotesProvider>
+        </ChakraProvider>
+      </DndProvider>
     </Router>
   </React.StrictMode>
 );
