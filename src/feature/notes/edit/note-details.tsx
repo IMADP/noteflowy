8950d2441eda91b7335fd5867200e2394d796903
Anthony DePalma <antdepalma@gmail.com>
2022-12-18 17:26:42 -0500

    code styling

--- src/feature/notes/edit/note-details.tsx
+++ src/feature/notes/edit/note-details.tsx
@@ -1,18 +1,19 @@
 import { IconButton } from '@chakra-ui/button';
 import { Box, Center, Divider, Flex, Stack, Text } from '@chakra-ui/layout';
 import { Tooltip } from '@chakra-ui/tooltip';
-import { useEffect, useMemo, useState } from 'react'
-import { BiArrowFromTop, BiArrowFromLeft, BiCopyAlt, BiBold, BiItalic, BiUnderline, BiStrikethrough, BiLinkAlt, BiEraser } from 'react-icons/bi';
+import { useCallback, useEffect, useMemo, useState } from 'react'
+import { BiArrowFromTop, BiArrowFromLeft, BiCopyAlt, BiBold, BiItalic, BiUnderline, BiStrikethrough, BiLinkAlt, BiEraser, BiCodeAlt } from 'react-icons/bi';
 import {
   BaseEditor,
   Descendant,
   createEditor,
-  Editor
+  Editor,
+  Transforms
 } from 'slate'
 import { Editable, ReactEditor, Slate, withReact } from 'slate-react';
 import { Note, useNotes } from '../use-notes';
 
-type CustomElement = { type: 'paragraph'; children: CustomText[] }
+type CustomElement = { type: 'paragraph' | 'code'; children: CustomText[] }
 type CustomText = { text: string }
 
 declare module 'slate' {
@@ -37,14 +38,36 @@ export const NoteDetails = ({ note }: NoteDetailsProps) => {
     editor.onChange();
   }, [note]);
 
+  const renderElement = useCallback((props: any) => {
+      switch (props.element.type) {
+      case 'code':
+        return <CodeElement {...props} />
+      case 'bold':
+        return <BoldElement {...props} />
+      default:
+        return <DefaultElement {...props} />
+    }
+  }, [])
+  
   return (
     <Slate editor={editor} value={details} onChange={(v) => setDetails(v)} >
       <Box tabIndex={1} px={4} py={3} borderRadius={8} border='1px' borderColor='gray.200'>
-        <Flex color='black'  mr="10">
+        <Flex color='black' mr="10">
           <Stack direction='row' spacing={1}>
 
-            <Tooltip hasArrow label='Bold'>
+          <Tooltip hasArrow label='Bold'>
               <IconButton
+              onClick={() => {
+                const [match] = Editor.nodes(editor, {
+                  match: n => Editor.isBlock(editor, n) && n.type === 'code'
+                })
+                // Toggle the block type depending on whether there's already a match.
+                Transforms.setNodes(
+                  editor,
+                  { type: match ? 'paragraph' : 'code' },
+                  { match: n => Editor.isBlock(editor, n) }
+                )
+              }}
                 size='xs'
                 variant='outline'
                 color='gray'
@@ -87,6 +110,27 @@ export const NoteDetails = ({ note }: NoteDetailsProps) => {
               <Divider color='black' orientation='vertical' />
             </Center>
 
+            <Tooltip hasArrow label='Code'>
+              <IconButton
+              onClick={() => {
+                const [match] = Editor.nodes(editor, {
+                  match: n => Editor.isBlock(editor, n) && n.type === 'code'
+                })
+                // Toggle the block type depending on whether there's already a match.
+                Transforms.setNodes(
+                  editor,
+                  { type: match ? 'paragraph' : 'code' },
+                  { match: n => Editor.isBlock(editor, n) }
+                )
+              }}
+                size='xs'
+                variant='outline'
+                color='gray'
+                aria-label='Bold'
+                icon={<BiCodeAlt />}
+              />
+            </Tooltip>
+
             <Tooltip hasArrow label='Link'>
               <IconButton
                 size='xs'
@@ -100,11 +144,32 @@ export const NoteDetails = ({ note }: NoteDetailsProps) => {
 
           </Stack>
         </Flex>
-        <Divider my='2'/>
+        <Divider my='2' />
         <Box color='grey'>
-          <Editable spellCheck={false} onBlur={() => notes.onUpdate({ ...note, details })} />
+          <Editable renderElement={renderElement}
+            spellCheck={false} onBlur={() => notes.onUpdate({ ...note, details })} />
         </Box>
       </Box>
     </Slate>
   );
+}
+
+const CodeElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <code>{props.children}</code>
+    </pre>
+  )
+}
+
+const BoldElement = (props: any) => {
+  return (
+    <pre {...props.attributes}>
+      <strong>{props.children}</strong>
+    </pre>
+  )
+}
+
+const DefaultElement = (props: any) => {
+  return <p {...props.attributes}>{props.children}</p>
 }
